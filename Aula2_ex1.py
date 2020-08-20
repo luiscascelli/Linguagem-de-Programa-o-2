d6ed415ae5942c5ab5ebb87d3533b86a63713f79
luiscascelli <luis.cascelli@gmail.com>
2020-08-20 15:40:05 -0300

    Exercicio 1

---
+++ Aula2_ex1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Editor Spyder
+
+Linguagem de programação 2 
+
+Aula 2 Ex1
+"""
+numeros = []
+for i in range(10):
+    print('Numero[',i,']')
+    numeros.append(int(input()))
