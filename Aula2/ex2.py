8fae99a1f3ee248df63cc3298388400a3ba148ab
luiscascelli <luis.cascelli@gmail.com>
2020-08-20 15:56:05 -0300

    exercicio 2

---
+++ Aula2/ex2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Editor Spyder
+
+Linguagem de programação 2 
+
+Aula 2 Ex2
+"""
+from random import randint
+
+lista = []
+for i in range (20):
+    lista.append(randint(0,100))
+
+print(lista)
